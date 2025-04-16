d55137bc9080c7a2395753a71eb9fe85dfdaa66d
aiturbidemil <aiturbide@milaboratories.com>
2025-04-16 18:37:44 +0200

    settings main page

--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 import '@milaboratories/graph-maker/styles';
-import { PlAlert, PlBlockPage, PlBtnGhost, PlDropdownRef, PlMaskIcon24, PlNumberField, PlSlideModal } from '@platforma-sdk/ui-vue';
+import { PlAlert, PlBlockPage, PlDropdownRef, PlNumberField } from '@platforma-sdk/ui-vue';
 import { useApp } from '../app';
-import { reactive } from 'vue';
 import type { PlRef } from '@platforma-sdk/model';
 import { plRefsEqual } from '@platforma-sdk/model';
 
 const app = useApp();
-
-const data = reactive<{
-  settingsOpen: boolean;
-}>({
-  settingsOpen: app.model.args.principalComponentsRef === undefined,
-});
 
 function setInput(inputRef?: PlRef) {
   app.model.args.principalComponentsRef = inputRef;
@@ -26,37 +19,27 @@ function setInput(inputRef?: PlRef) {
 
 <template>
   <PlBlockPage>
-    <template #title>Leiden Clustering</template>
-    <template #append>
-      <PlBtnGhost @click.stop="() => data.settingsOpen = true">
-        Settings
-        <template #append>
-          <PlMaskIcon24 name="settings" />
-        </template>
-      </PlBtnGhost>
-    </template>
-
-    <PlSlideModal v-model="data.settingsOpen">
-      <template #title>Settings</template>
-      <PlDropdownRef
-        v-model="app.model.args.principalComponentsRef" :options="app.model.outputs.embeddingOptions"
-        label="Select dataset"
-        clearable @update:model-value="setInput"
-      />
-      <PlNumberField
-        v-model="app.model.args.resolution"
-        label="Resolution" :minValue="0.1" :step="0.1"
-      >
-        <template #tooltip>
-          Select resolution for Leiden clustering. The bigger the resolution, the more clusters will be found.
-        </template>
-      </PlNumberField>
-      <PlAlert v-if="app.model.args.resolution > 1" type="warn">
-        {{ "Warning: The selected resolution is over commonly used range (0.4 - 1.0)" }}
-      </PlAlert>
-      <PlAlert v-if="app.model.args.resolution < 0.4" type="warn">
-        {{ "Warning: The selected resolution is under commonly used range (0.4 - 1.0)" }}
-      </PlAlert>
-    </PlSlideModal>
+    <template #title>Settings</template>
+    <PlDropdownRef
+      v-model="app.model.args.principalComponentsRef" :options="app.model.outputs.embeddingOptions"
+      :style="{ width: '320px' }"
+      label="Select dataset"
+      clearable @update:model-value="setInput"
+    />
+    <PlNumberField
+      v-model="app.model.args.resolution"
+      :style="{ width: '320px' }"
+      label="Resolution" :minValue="0.1" :step="0.1"
+    >
+      <template #tooltip>
+        Select resolution for Leiden clustering. The bigger the resolution, the more clusters will be found.
+      </template>
+    </PlNumberField>
+    <PlAlert v-if="app.model.args.resolution > 1" type="warn">
+      {{ "Warning: The selected resolution is over commonly used range (0.4 - 1.0)" }}
+    </PlAlert>
+    <PlAlert v-if="app.model.args.resolution < 0.4" type="warn">
+      {{ "Warning: The selected resolution is under commonly used range (0.4 - 1.0)" }}
+    </PlAlert>
   </PlBlockPage>
 </template>
